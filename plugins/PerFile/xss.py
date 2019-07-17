c610fc46ecf1d8587c81d843c5f4fb869a70b888
boy-hack <34109680@qq.com>
2019-07-17 15:58:48 +0800

    多种方式检测XSS

--- plugins/PerFile/xss.py
+++ plugins/PerFile/xss.py
@@ -3,10 +3,11 @@
 # @Time    : 2019/7/15 3:52 PM
 # @Author  : w8ay
 # @File    : xss.py
-
+# referer: https://www.anquanke.com/post/id/148357
 import copy
 import os
 import random
+import re
 
 import requests
 
@@ -17,7 +18,7 @@ from lib.plugins import PluginBase
 
 
 class W13SCAN(PluginBase):
-    name = 'XSS简易探测'
+    name = 'XSS多种方式探测'
     desc = '''暂只支持Get请求方式'''
 
     def audit(self):
@@ -42,15 +43,71 @@ class W13SCAN(PluginBase):
 
             rndStr = 9000 + random.randint(1, 999)
             tag = random_str(4)
-            payload = "<{tag}>{randint}</{tag}>".format(tag=tag, randint=rndStr)
+            html_payload = "<{tag}>{randint}</{tag}>".format(tag=tag, randint=rndStr)  # html xss
+            attr_payload = [
+                '" oNsOmeEvent="console.log(233)',  # 双引号payload
+                "' oNsOmeEvent='console.log(2333)",  # 单引号payload
+            ]
+            url_payload = "javascript:{randint}".format(randint=rndStr)
+            javascript_payload = [
+                ""
+            ]
 
             for k, v in params.items():
                 if k.lower() in ignoreParams:
                     continue
                 data = copy.deepcopy(params)
-                data[k] = v + payload
+                ranstr = random_str(5)
+                data[k] = v + ranstr
                 r = requests.get(url, headers=headers, params=data)
                 html1 = r.text
-                if payload in html1:
-                    out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw, descript="探测tag被解析")
-                    break
+                if ranstr not in html1:
+                    continue
+
+                in_script = False
+                script_group = re.findall('<script.*?>(.*?)</script>', html1, re.I | re.S | re.M)
+                if script_group:
+                    for i in script_group:
+                        if ranstr in i:
+                            in_script = True
+                            break
+
+                if in_script:
+                    out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw,
+                                descript="探测字符在<script>脚本内被解析",
+                                type="javascript xss")
+                else:
+                    # check html xss
+                    data[k] = v + html_payload
+                    r = requests.get(url, headers=headers, params=data)
+                    html1 = r.text
+                    if html_payload in html1:
+                        out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw, descript="探测tag被解析",
+                                    type="html xss")
+                        break
+
+                    # check attr xss
+                    for payload in attr_payload:
+                        data[k] = v + payload
+                        r = requests.get(url, headers=headers, params=data)
+                        html1 = r.text
+                        if payload[0] == '"':
+                            if payload + '"' in html1:
+                                out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw,
+                                            type="标签属性xss")
+                                break
+                        elif payload[0] == "'":
+                            if payload + "'" in html1:
+                                out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw,
+                                            type="标签属性xss")
+                                break
+
+                    # check url payload
+                    re_parren = r'''[src|href|action]=['"]'''
+                    data[k] = v + url_payload
+                    r = requests.get(url, headers=headers, params=data)
+                    html1 = r.text
+                    if re.search(re_parren + url_payload, html1):
+                        out.success(url, self.name, payload="{}:{}".format(k, data[k]), raw=r.raw,
+                                    type="url xss")
+                        break
