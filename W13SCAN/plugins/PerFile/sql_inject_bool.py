3e657315164835df7504e24896bebf2acf6a7a23
boy-hack <34109680@qq.com>
2019-09-07 00:47:18 +0800

    优化SQL布尔盲注，多重判断

--- W13SCAN/plugins/PerFile/sql_inject_bool.py
+++ W13SCAN/plugins/PerFile/sql_inject_bool.py
@@ -60,18 +60,82 @@ class W13SCAN(PluginBase):
 
         return page
 
+    def inject(self, params, k, payload_false, payload_true):
+        data = copy.deepcopy(params)
+        is_inject = False
+
+        data[k] = payload_false
+        r2 = requests.get(self.netloc, params=data, headers=self.headers)
+        falsePage = self.removeDynamicContent(r2.text)
+
+        try:
+            self.seqMatcher.set_seq1(self.resp_str)
+            self.seqMatcher.set_seq2(falsePage)
+            ratio_false = round(self.seqMatcher.quick_ratio(), 3)
+            # ratio *= GetRatio(resp_str, html1)
+            if ratio_false == 1.0:
+                return False
+        except (MemoryError, OverflowError):
+            return False
+
+        # true page
+
+        data[k] = payload_true
+        r = requests.get(self.netloc, params=data, headers=self.headers)
+        truePage = self.removeDynamicContent(r.text)
+
+        if truePage == falsePage:
+            return False
+
+        try:
+            self.seqMatcher.set_seq1(self.resp_str or "")
+            self.seqMatcher.set_seq2(truePage or "")
+            ratio_true = round(self.seqMatcher.quick_ratio(), 3)
+        except (MemoryError, OverflowError):
+            return False
+
+        if ratio_true > self.UPPER_RATIO_BOUND - 1 and (ratio_true - ratio_false) > self.DIFF_TOLERANCE:
+            if ratio_false <= self.UPPER_RATIO_BOUND:
+                is_inject = True
+        if not is_inject and ratio_true > 0.68 and abs(ratio_true - ratio_false) > 0.05:
+            originalSet = set(getFilteredPageContent(self.resp_str, True, "\n").split("\n"))
+            trueSet = set(getFilteredPageContent(truePage, True, "\n").split("\n"))
+            falseSet = set(getFilteredPageContent(falsePage, True, "\n").split("\n"))
+
+            if len(originalSet - trueSet) <= 2 and trueSet != falseSet:
+                candidates = trueSet - falseSet
+                if len(candidates) > 0:
+                    is_inject = True
+                # if candidates:
+                #     candidates = sorted(candidates, key=len)
+                #     for candidate in candidates:
+                #         if re.match(r"\A[\w.,! ]+\Z",
+                #                     candidate) and ' ' in candidate and candidate.strip() and len(
+                #             candidate) > 10:
+                #             is_inject = True
+                #             break
+
+        if is_inject:
+            infoMsg = "false_ratio:{} true_ratio:{} ".format(ratio_false, ratio_true)
+            return {
+                "requests": [r2.raw, r.raw],
+                "info": infoMsg
+            }
+        else:
+            return False
+
     def audit(self):
         method = self.requests.command  # 请求方式 GET or POST
-        headers = self.requests.get_headers()  # 请求头 dict类型
+        self.headers = headers = self.requests.get_headers()  # 请求头 dict类型
         url = self.build_url()  # 请求完整URL
 
         resp_data = self.response.get_body_data()  # 返回数据 byte类型
-        resp_str = self.response.get_body_str()  # 返回数据 str类型 自动解码
+        self.resp_str = resp_str = self.response.get_body_str()  # 返回数据 str类型 自动解码
         resp_headers = self.response.get_headers()  # 返回头 dict类型
 
         p = self.requests.urlparse
         params = self.requests.params
-        netloc = self.requests.netloc
+        self.netloc = self.requests.netloc
 
         if method == 'GET':
             # 从源码中获取更多链接
@@ -106,12 +170,12 @@ class W13SCAN(PluginBase):
                 "'&&'{0}'='{1}",
                 '"&&"{0}"="{1}',
             ]
+
             for k, v in params.items():
                 if k.lower() in ignoreParams:
                     continue
-                data = copy.deepcopy(params)
+
                 for flag in sql_flag:
-                    is_inject = False
                     is_num = False
                     if flag == "<--isdigit-->":
                         if str(v).isdigit():
@@ -122,63 +186,28 @@ class W13SCAN(PluginBase):
                         payload_false = "{}/0".format(v)
                     else:
                         payload_false = v + flag.format(random_str(1) + 'a', random_str(1) + 'b')
-                    data[k] = payload_false
-                    r2 = requests.get(netloc, params=data, headers=headers)
-                    falsePage = self.removeDynamicContent(r2.text)
 
-                    try:
-                        self.seqMatcher.set_seq1(resp_str)
-                        self.seqMatcher.set_seq2(falsePage)
-                        ratio_false = round(self.seqMatcher.quick_ratio(), 3)
-                        # ratio *= GetRatio(resp_str, html1)
-                        if ratio_false == 1.0:
-                            continue
-                    except (MemoryError, OverflowError):
-                        continue
-
-                    # true page
                     rand_str = random_str(2)
                     if is_num:
                         payload_true = "{}*1".format(v)
                     else:
                         payload_true = v + flag.format(rand_str, rand_str)
-                    data[k] = payload_true
-                    r = requests.get(netloc, params=data, headers=headers)
-                    truePage = self.removeDynamicContent(r.text)
 
-                    if truePage == falsePage:
-                        continue
+                    ret1 = self.inject(params, k, payload_false, payload_true)
+                    if ret1:
+                        if is_num:
+                            payload_false = "{}/10000".format(v)
+                        else:
+                            payload_false = v + flag.format(random_str(1) + 'c', random_str(1) + 'a')
 
-                    try:
-                        self.seqMatcher.set_seq1(resp_str or "")
-                        self.seqMatcher.set_seq2(truePage or "")
-                        ratio_true = round(self.seqMatcher.quick_ratio(), 3)
-                    except (MemoryError, OverflowError):
-                        continue
+                        rand_str = random_str(2)
+                        if is_num:
+                            payload_true = "{}*1".format(v)
+                        else:
+                            payload_true = v + flag.format(rand_str, rand_str)
 
-                    if ratio_true > self.UPPER_RATIO_BOUND - 1 and (ratio_true - ratio_false) > self.DIFF_TOLERANCE:
-                        if ratio_false <= self.UPPER_RATIO_BOUND:
-                            is_inject = True
-                    if not is_inject and ratio_true > 0.68 and abs(ratio_true - ratio_false) > 0.05:
-                        originalSet = set(getFilteredPageContent(resp_str, True, "\n").split("\n"))
-                        trueSet = set(getFilteredPageContent(truePage, True, "\n").split("\n"))
-                        falseSet = set(getFilteredPageContent(falsePage, True, "\n").split("\n"))
-
-                        if len(originalSet - trueSet) <= 2 and trueSet != falseSet:
-                            candidates = trueSet - falseSet
-                            if len(candidates) > 0:
-                                is_inject = True
-                            # if candidates:
-                            #     candidates = sorted(candidates, key=len)
-                            #     for candidate in candidates:
-                            #         if re.match(r"\A[\w.,! ]+\Z",
-                            #                     candidate) and ' ' in candidate and candidate.strip() and len(
-                            #             candidate) > 10:
-                            #             is_inject = True
-                            #             break
-
-                    if is_inject:
-                        infoMsg = "false_ratio:{} true_ratio:{} ".format(ratio_false, ratio_true)
-                        out.success(url, self.name, raw=[r2.raw, r.raw], payload_true=k + ":" + payload_true,
-                                    payload_false=k + ":" + payload_false, infoMsg=infoMsg)
-                        break
+                        ret2 = self.inject(params, k, payload_false, payload_true)
+                        if ret2:
+                            out.success(url, self.name, raw=ret2["requests"], payload_true=k + ":" + payload_true,
+                                        payload_false=k + ":" + payload_false, infoMsg=ret2["info"])
+                            return True
