d0fa5bf0801e49d0783d4259ca52de1ebc0432b5
boy-hack <34109680@qq.com>
2019-10-10 20:42:45 +0800

    update test http smuggling

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,11 +10,18 @@ from W13SCAN.api import Scanner
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.scanner = Scanner(threads=10)
+        pass
 
     def tearDown(self):
         pass
 
     def test_api_get(self):
+        self.scanner = Scanner(threads=10)
         self.scanner.put("http://emlog6.demo/?post=1")
         self.scanner.run()
+
+    def test_http_smuggling(self):
+        url = "https://acfe1f111e6d561480049808007c0038.web-security-academy.net/"
+        scan = Scanner(include_plugins=['http_smuggling.py'])
+        scan.put(url)
+        scan.run()
